83dc8fbe10b38f876fff61eb2640906c2ed1f6ab
peggypeggyq <qiu.p@husky.neu.edu>
2020-06-05 13:01:49 -0400

    Resolve coverage issue (#35)
    * issue-08 added user tests for user update and validation check
    
    * issue-08 added mock user tests
    
    * issue-8 ran sonarqube and removed code smells/bugs in user related classes
    
    * issue-08 added tests
    
    * issue-8-basic-user added close connection test
    
    * update group mock tests
    
    * controller test
    
    * issue-8-added-coverage
    
    * issue-8-basic-user added missing methods
    
    Co-authored-by: qjgong <gong.q@husky.neu.edu>
    Co-authored-by: yinrouni <yin.ro@husky.neu.edu>

--- Development/src/test/java/com/neu/prattle/AllTests.java
+++ Development/src/test/java/com/neu/prattle/AllTests.java
@@ -1,12 +1,11 @@
 package com.neu.prattle;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
 @RunWith(Suite.class)
-<<<<<<< HEAD
-@SuiteClasses({GroupEntityTest.class, GroupServiceTest.class, UserTests.class, UserServiceTests.class, UserServiceMockTests.class})
-=======
-@SuiteClasses({GroupEntityTest.class, GroupServiceMockTests.class, UserTests.class, UserMockTests.class})
->>>>>>> f9a804a3f20a67db0a0c324f7c55e43f80f54ff6
+@SuiteClasses({GroupEntityTest.class, UserControllerTest.class, GroupControllerTest.class, GroupServiceMockTests.class, UserTests.class, UserServiceTests.class, UserServiceMockTests.class})
+
 public class AllTests {
 }
