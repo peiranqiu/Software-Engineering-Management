5133ade78dd84c1b604fc20afb70466d338df012
Daniehao <hao.yij@husky.neu.edu>
2020-06-05 00:35:17 -0400

    update storage

--- Development/src/main/java/com/neu/prattle/model/Message.java
+++ Development/src/main/java/com/neu/prattle/model/Message.java
@@ -1,12 +1,17 @@
 package com.neu.prattle.model;
 
+import com.neu.prattle.websocket.MessageEncoder;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.UUID;
+
+import javax.websocket.EncodeException;
 
 /***
  * A Basic POJO for Message.
@@ -93,25 +98,45 @@ public class Message {
 
     public int getToID() {return toID;}
 
-    public boolean storeMessage() {
+    public void makeDirectory(String messagePath, int userID) {
+        File fromUserFile = new File(messagePath + "/" + userID);
+        fromUserFile.mkdir();
+        File fileUser = new File(messagePath + "/" + userID + "/messageSent");
+        fileUser.mkdir();
+        File fileSender = new File(messagePath + "/" + userID +"/messageReceived");
+        fileSender.mkdir();
+    }
+
+    public boolean storeMessage() throws IOException, EncodeException {
         if (fromID != -1 && toID != -1 && !content.isEmpty() && !from.isEmpty() && !to.isEmpty()) {
             String path = Message.class.getResource("").getPath();
             String mainPath = path.substring(0, path.indexOf("main") + 4);
             String messagePath = mainPath + "/messages";
-            if (!Files.exists(Paths.get(messagePath+fromID))){
-                File fileUser = new File(messagePath + "messageSent");
-                fileUser.mkdir();
-                File fileSender = new File(messagePath + "messageReceived");
-                fileSender.mkdir();
+            if (!Files.exists(Paths.get(messagePath + "/" + fromID))){
+                makeDirectory(messagePath, fromID);
             }
-            if (!Files.exists(Paths.get(messagePath+toID))){
-                File fileUser = new File(messagePath + "messageSent");
-                fileUser.mkdir();
-                File fileSender = new File(messagePath + "messageReceived");
-                fileSender.mkdir();
+            if (!Files.exists(Paths.get(messagePath + "/" + toID))){
+                makeDirectory(messagePath, toID);
+            }
+            File file1 = new File(messagePath + "/" + fromID + "messageSent" + "/" + messageID + ".txt");
+            File file2 = new File(messagePath + "/" + toID  + "messageReceived" + "/" + messageID + ".txt");
+            if (file1.createNewFile() && file2.createNewFile()) {
+                writeFile(messagePath);
+                return true;
             }
         }
         return false;
+    }
+
+    private void writeFile(String messagePath) throws IOException, EncodeException {
+        MessageEncoder msEncoder = new MessageEncoder();
+        FileWriter myWriter = new FileWriter(messagePath + "/" + fromID + "messageSent" + "/" + messageID + ".txt");
+        myWriter.write(msEncoder.encode(this));
+        myWriter.close();
+        MessageEncoder msEncoder2 = new MessageEncoder();
+        FileWriter myWriter2 = new FileWriter(messagePath + "/" + toID  + "messageReceived" + "/" + messageID + ".txt");
+        myWriter2.write(msEncoder2.encode(this));
+        myWriter2.close();
     }
 
     public static MessageBuilder messageBuilder()   {
