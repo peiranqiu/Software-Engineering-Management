62acadab4fcc8dd8c95eab712b4b7d43c2032596
qjgong <qjgong79@gmail.com>
2020-06-11 22:26:07 -0400

    update test6 in groupService test

--- Development/src/main/java/com/neu/prattle/service/DBUtils.java
+++ Development/src/main/java/com/neu/prattle/service/DBUtils.java
@@ -15,8 +15,8 @@ import java.util.logging.Logger;
 public abstract class DBUtils {
 
   private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-//  protected String url = "jdbc:mysql://mydb.cd4ztimoe6ek.us-east-1.rds.amazonaws.com:3306/mydb?";
-  protected String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=EST5EDT";
+  protected String url = "jdbc:mysql://mydb.cd4ztimoe6ek.us-east-1.rds.amazonaws.com:3306/mydb?";
+//  protected String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=EST5EDT";
   protected String user = "mydb";
   protected String pd = "CS5500team4";
   protected Connection con = null;
