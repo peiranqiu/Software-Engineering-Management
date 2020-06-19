b20eb46a7175c46202506fc9cf67cbfd1b76e8be
peiranqiu <qiu.p@husky.neu.edu>
2020-06-18 21:50:17 -0400

    delete onerror

--- Development/src/main/java/com/neu/prattle/websocket/ChatEndpoint.java
+++ Development/src/main/java/com/neu/prattle/websocket/ChatEndpoint.java
@@ -27,7 +27,6 @@ import java.util.logging.Logger;
 
 import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
-import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
