37e41257d7e329dfa4541969b6a3fab0b8b22c0d
Daniehao <hao.yij@husky.neu.edu>
2020-06-23 13:39:39 -0400

    add database on message update

--- Development/src/test/java/com/neu/prattle/AllTests.java
+++ Development/src/test/java/com/neu/prattle/AllTests.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * This is a wrapper for all test classes.
  */
 @RunWith(Suite.class)
-@SuiteClasses({GroupEntityTest.class, MessageEntityTest.class, GroupMockitoTest.class,
+@SuiteClasses({GroupEntityTest.class, MessageEntityTestOld.class, GroupMockitoTest.class,
         UserEntityTest.class, UserMockitoTest.class, FollowMockitoTest.class, ChatEndpointMockitoTest.class, ModerateMockitoTest.class})
 public class AllTests {
   //All tests
