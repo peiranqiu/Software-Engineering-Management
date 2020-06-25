3956d3f9c7a50334499f3c91adab20ba82ff5647
peiranqiu <qiu.p@husky.neu.edu>
2020-06-25 02:07:38 -0400

    modified watch log

--- Development/src/main/java/com/neu/prattle/controller/UserController.java
+++ Development/src/main/java/com/neu/prattle/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.neu.prattle.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import com.neu.prattle.model.Group;
 import com.neu.prattle.model.Message;
@@ -12,6 +13,7 @@ import com.neu.prattle.service.ModerateService;
 import com.neu.prattle.service.UserService;
 import com.neu.prattle.service.UserServiceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -132,7 +134,20 @@ public final class UserController {
   @Consumes(MediaType.APPLICATION_JSON)
   public String watchUser(@PathParam("userId") int userId) {
     String username = userService.findUserById(userId).getName();
-    return new Gson().toJson(messageService.getUserLog(username));
+    List<Message> list = messageService.getUserLog(username);
+    StringBuilder sb = new StringBuilder();
+    sb.append("Logs for user @" + username + " ");
+    for(Message m: list) {
+      sb.append("[sent to ");
+      sb.append(m.getSendToGroup()? "group " : "user ");
+      sb.append(m.getTo());
+      sb.append(" at ");
+      sb.append(m.getTimeStamp());
+      sb.append(": \"");
+      sb.append(m.getContent());
+      sb.append("\"] ");
+    }
+    return sb.toString();
   }
 
   /**
